4a1bd040c3fd4752573a6bb8546180b84a91d263
minhtien2903 <43950811+minhtien2903@users.noreply.github.com>
2019-07-07 13:16:10 +0700

    Delete deny.php

--- deny.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'config.php';
-    if (isset($_POST['action']) && $_POST["action"] == "deny"){
-        $user_data = $_SESSION['userData'];
-        $maSV = $user_data['maSV'];
-        $check = mysqli_query($con, "DELETE FROM dang_ky  WHERE maSV ='$maSV' and ttDK !=4");
-    }
-?>
